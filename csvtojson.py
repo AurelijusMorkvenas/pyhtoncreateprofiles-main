65842d0261ada9cf84cd948a8e458613d948d75b
AurelijusMorkvenas <aurelijus.morkvenas@yh.nackademin.se>
2024-05-04 15:25:30 +0200

    insert

---
+++ csvtojson.py
@@ -0,0 +1,27 @@
+import csv
+import json
+import os
+
+def csv_to_json(csv_file, json_file):
+    # Ange den fullständiga sökvägen till CSV-filen
+    csv_file_path = os.path.join(os.getenv('GITHUB_WORKSPACE', '.'), csv_file)
+
+    # Öppna CSV-filen och läs innehållet med rätt teckenkodning
+    with open(csv_file_path, 'r', encoding='utf-8-sig') as file:
+        reader = csv.DictReader(file)
+        data = list(reader)
+
+    # Ange den fullständiga sökvägen till JSON-filen
+    json_file_path = os.path.join(os.getenv('GITHUB_WORKSPACE', '.'), json_file)
+
+    # Skriv JSON-data till json-filen med UTF-8 teckenkodning
+    with open(json_file_path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+#Ange sökvägen till CSV- och JSON-filerna
+csv_file = 'profiles1.csv'
+json_file = 'data.json'
+
+#Konvertera CSV till JSON
+csv_to_json(csv_file, json_file)
+
---
+++ tests.py
@@ -0,0 +1,49 @@
+import unittest
+import csv
+import json
+
+class TestFileContents(unittest.TestCase):
+
+    def testcsvcolumns(self):
+        with open('profiles1.csv', 'r') as file:
+            reader = csv.reader(file)
+            columns = next(reader)
+            self.assertEqual(len(columns), 12)
+
+    def testcsvrows(self):
+        with open('profiles1.csv', 'r') as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+            self.assertTrue(len(rows) >= 900)
+
+    def test_json_properties(self):
+        with open('data.json', 'r') as file:
+            data = json.load(file)
+            print("Data in JSON file:", data)
+            expected_properties = [
+                "Givenname",
+                "Surname",
+                "Streetaddress",
+                "City",
+                "Zipcode",
+                "Country",
+                "CountryCode",
+                "NationalId",
+                "TelephoneCountryCode",
+                "Telephone",
+                "Birthday",
+                "ConsentToContact"
+            ]
+            print("Expected properties:", expected_properties)
+            for obj in data:
+                print("Object properties:", obj.keys())
+            self.assertTrue(all(prop in obj for obj in data for prop in expected_properties))
+
+    def test_json_rows(self):
+        with open('data.json', 'r') as file:
+            data = json.load(file)
+            self.assertTrue(len(data) >= 900)
+
+
+if __name__== '__main__':
+    unittest.main()       
