55108ec9d6de1914900563b7c06b42a014510f2e
AurelijusMorkvenas <aurelijus.morkvenas@yh.nackademin.se>
2024-05-14 21:01:59 +0200

    looks better

--- csvtojson.py
+++ csvtojson.py
@@ -3,24 +3,24 @@ import json
 import os
 
 def csv_to_json(csv_file, json_file):
-    # Ange den fullständiga sökvägen till CSV-filen
+    # Specify the full path to the CSV file
     csv_file_path = os.path.join(os.getenv('GITHUB_WORKSPACE', '.'), csv_file)
 
-    # Öppna CSV-filen och läs innehållet med rätt teckenkodning
+    # Open the CSV file and read the contents with the correct encoding
     with open(csv_file_path, 'r', encoding='utf-8-sig') as file:
         reader = csv.DictReader(file)
         data = list(reader)
 
-    # Ange den fullständiga sökvägen till JSON-filen
+    # Specify the full path to the JSON file
     json_file_path = os.path.join(os.getenv('GITHUB_WORKSPACE', '.'), json_file)
 
-    # Skriv JSON-data till json-filen med UTF-8 teckenkodning
+    # Write JSON data to the JSON file with UTF-8 encoding
     with open(json_file_path, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
-#Ange sökvägen till CSV- och JSON-filerna
+# Specify the path to the CSV and JSON files
 csv_file = 'profiles1.csv'
 json_file = 'data.json'
 
-#Konvertera CSV till JSON
+# Convert CSV to JSON
 csv_to_json(csv_file, json_file)
--- tests.py
+++ tests.py
@@ -5,18 +5,21 @@ import json
 class TestFileContents(unittest.TestCase):
 
     def testcsvcolumns(self):
+        # Open the CSV file and verify that it contains 12 columns
         with open('profiles1.csv', 'r') as file:
             reader = csv.reader(file)
             columns = next(reader)
             self.assertEqual(len(columns), 12)
 
     def testcsvrows(self):
+        # Open the CSV file and verify that it contains 900+ rows
         with open('profiles1.csv', 'r') as file:
             reader = csv.reader(file)
             rows = list(reader)
             self.assertTrue(len(rows) >= 900)
 
     def test_json_properties(self):
+        # Open the JSON file and verify that it contains all required properties
         with open('data.json', 'r') as file:
             data = json.load(file)
             print("Data in JSON file:", data)
@@ -40,6 +43,7 @@ class TestFileContents(unittest.TestCase):
             self.assertTrue(all(prop in obj for obj in data for prop in expected_properties))
 
     def test_json_rows(self):
+        # Open the JSON file and verify that it contains 900+ rows
         with open('data.json', 'r') as file:
             data = json.load(file)
             self.assertTrue(len(data) >= 900)
